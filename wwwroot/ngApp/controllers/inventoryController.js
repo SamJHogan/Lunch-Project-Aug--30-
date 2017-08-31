392dc52f530eaecaac71071d8aa569f6add753cf
Joy Kimaru <Joy Kimaru>
2017-08-30 17:39:48 -0700

    changes to inventory

--- wwwroot/ngApp/controllers/inventoryController.js
+++ wwwroot/ngApp/controllers/inventoryController.js
@@ -1,16 +1,18 @@
 class InventoryController {
     constructor() {
-        this.getInventory()
+        
         this.message = "hello from inventory";
-        let cars = this.getInventory();
+        this.cars = [{Year:2017, Make:"Ford", Model:"F-150", Price:23295},
+            {Year:2017, Make:"Ford", Model:"Focus", Price:14950},
+            {Year:2017, Make:"Ford", Model:"Escape", Price:22195},
+            {Year:2017, Make:"Toyota", Model:"Corolla", Price:12950},
+            {Year:2017, Make:"Toyota", Model:"Camry", Price:18763},
+            {Year:2017, Make:"Nissan", Model:"Altima", Price:19503}]; 
+
+        this.getInventory()
     }
     getInventory() {
-        return[{Year:2017, Make:"Ford", Model:"F-150", Price:23295},
-        {Year:2017, Make:"Ford", Model:"Focus", Price:14950},
-        {Year:2017, Make:"Ford", Model:"Escape", Price:22195},
-        {Year:2017, Make:"Toyota", Model:"Corolla", Price:12950},
-        {Year:2017, Make:"Toyota", Model:"Camry", Price:18763},
-        {Year:2017, Make:"Nissan", Model:"Altima", Price:19503}];       
+        return this.cars;     
                 
     }
 } 
