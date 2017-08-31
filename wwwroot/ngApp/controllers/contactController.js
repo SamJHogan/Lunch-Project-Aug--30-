e64c1c379aeb8f1785da9e26f505a53f15672778
Joy Kimaru <Joy Kimaru>
2017-08-30 17:02:46 -0700

    some changes

--- wwwroot/ngApp/controllers/contactController.js
+++ wwwroot/ngApp/controllers/contactController.js
@@ -1,5 +1,10 @@
 class ContactController {
     constructor() {
-        this.message = "hello from Contact";
+        this.message = "Please leave a message and an agent will contact you as soon as they are able:";
     }
+    // click(a,b){
+    //     let message = [{Name:'',Message:''}];
+
+    //     message.push(a,b);
+    // }
 }
--- wwwroot/ngApp/controllers/inventoryController.js
+++ wwwroot/ngApp/controllers/inventoryController.js
@@ -1,5 +1,11 @@
 class InventoryController {
     constructor() {
         this.message = "hello from inventory";
+        let cars = this.getInventory();
+    }
+    getInventory(){
+         return [{Year: 2015, Make: "Nissan", Model: "Rouge", Price: 201545}];
+        
+        
     }
 }
